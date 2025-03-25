1df63364b406a5cb648972c6ba513f25f2a581ff
1iuyu <145678619+1iuyu@users.noreply.github.com>
2025-03-25 16:18:50 +0800

    Update matlab_wrapper.m

--- matlab_wrapper.m
+++ matlab_wrapper.m
@@ -7,7 +7,7 @@ C = textscan(fid,'%n%s');
 fclose(fid);
 
 % set design variables -- could use vector notation
-spf = "path\to\soil parameter file";
+spf = "path\to\soil parameter file"; % 修改土壤文件地址
 files = dir(fullfile(spf, 'soil*.txt'));
 for i = 1:length(files)
     soils_name = fullfile(spf, files(i).name);
@@ -24,29 +24,42 @@ for i = 1:length(files)
 end
 
 %% run vic model
-system("wsl ./shell_script.sh"); 
-%% Calculate summed runoff and baseflow
+maxRetries = 10; % 最大重试次数
+retryCount = 0; % 当前重试次数
+ans = 1; % 初始化状态为失败
+while ans ~= 0 && retryCount < maxRetries
+    ans = system("wsl ./shell_script.sh");
+    retryCount = retryCount + 1;
+    pause(1); % 可选：在重试之间暂停一段时间
+end
 
-wb=load("path\to\rout_parameter file");
-q_m=wb(:,3)./35.315; %converted to m³/s
+%% Calculate summed runoff and baseflow
+retryCount1 = 0; % 当前重试次数
+ans = 1; % 初始化状态为失败
+while ans ~= 0 && retryCount1 < maxRetries
+    ans = system("wsl /mnt/d/VIC_Routing-Fortran_version/src/rout /path/to/rout_input.txt"); % 修改汇流输入文件地址
+    retryCount1 = retryCount1 + 1;
+    pause(1);
+end
+wb=load("path\to\rout_parameter file"); % 修改汇流文件地址
+q_m=wb(:,3)./35.315; % converted to m³/s
 %% Load in validation data
   
-valdata = "path\to\measured data file";
+valdata = "path\to\measured data file"; % 修改验证数据地址
 truth = readmatrix(valdata);
 q = truth(:,1);
-f = -1*myNSE(q, q_m);
-t=1:length(q_m);
-lw = 1.5;
-figure, plot(t, q_m, 'linewidth', lw)
-hold on, plot(t,q, 'linewidth', lw)
-legend('pred','obs')
-xlabel('Time')
-ylabel('Monthly streamflow (m^3/s)')
-set(gca, 'fontsize', 16)
+f1 = -1*myNSE(q, q_m);
+f2 = -1*myKGE(q, q_m);
+% t=1:length(q_m);
+% lw = 1.5;
+% figure, plot(t, q_m, 'linewidth', lw)
+% hold on, plot(t,q, 'linewidth', lw)
+% legend('pred','obs')
+% xlabel('Time')
+% ylabel('Monthly streamflow (m^3/s)')
+% set(gca, 'fontsize', 16)
 
 %% WRITE results.out
 fid = fopen(results,'w');
-fprintf(fid,'%20.10e     f\n', f);
-%fprintf(fid,'%20.10e     params\n', C{1}(2:5));
-
+fprintf(fid,'%20.10e f1 %20.10e f2\n', f1, f2);
 fclose(fid);
